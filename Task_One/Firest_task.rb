e421410d9b58118ba60680de6e16095d501b6746
Balu <barishvili@unisens.ge>
2022-03-28 12:05:46 +0400

    corrected

--- Task_One/Firest_task.rb
+++ Task_One/Firest_task.rb
@@ -1,12 +1,13 @@
+OPTIMAL = 110
 class IdealWeight
-    def ideal_wright(name,height)
-        if height.to_i-110 <= 0
-           print "#{name} Your weight is already optimal"
-        else
-           optimal_weight = height.to_i - 110 
-            print "#{name} Your optimal weight is #{optimal_weight}"
-        end
+  def ideal_wright(name,height)
+    if height.to_i - OPTIMAL <= 0
+      print "#{name} Your weight is already optimal"
+    else
+      optimal_weight = height.to_i - OPTIMAL 
+      print "#{name} Your optimal weight is #{optimal_weight}"
     end
+  end
 end
 
 idealweight = IdealWeight.new
