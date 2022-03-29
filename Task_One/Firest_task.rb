85e3c50cc92d750c4d1ac3590afe264566b0ac40
Balu <barishvili@unisens.ge>
2022-03-29 11:17:48 +0400

    corrected First Task

--- Task_One/Firest_task.rb
+++ Task_One/Firest_task.rb
@@ -1,5 +1,11 @@
-OPTIMAL = 110
+#Ideal weight. The program asks for name and height and displays the ideal weight using the formula <height> - 110,
+#then displays the result to the user by name. If the ideal weight is negative, the line "Your weight is already optimal" is displayed
+#Screen reader support enabled.
+
+
 class IdealWeight
+  OPTIMAL = 110
+  
   def ideal_wright(name,height)
     if height.to_i - OPTIMAL <= 0
       print "#{name} Your weight is already optimal"
