83a046c25a36cb422e164688d713306282c293b0
Balu <barishvili@unisens.ge>
2022-03-30 15:29:36 +0400

    Purchase amount

---
+++ Task_One/Tenth_task.rb
@@ -0,0 +1,56 @@
+ZERO = 0
+
+class Purchase
+
+  def initialize
+    @purchase = {}
+    @amount = {}
+  end
+  
+  def add_in_cart
+    loop do 
+      puts "\n When you end adding product in your cart enter 'stop'"
+      puts "\nEnter product name"
+      name = gets.chomp
+      if name == "stop"
+        break
+      end
+      puts "Enter product price"
+      price = gets.chomp.to_f
+      puts "Enter product quantity"
+      quantity = gets.chomp.to_f
+      @purchase[name] = {price: price, quantity: quantity}
+    end
+  end
+
+  def create_amount
+    @purchase.each do |key, value| 
+      @amount[key] = value[:price] * value[:quantity]
+    end
+  end
+
+  def get_amount
+    @amount.each { |key, value| 
+      if value == ZERO  
+        "no product" 
+      else 
+        puts "#{key} total amount is #{value}" 
+      end}
+  end
+
+  def get_amount_sum
+    if @amount.values.sum == ZERO
+      "empty cart"
+    else
+      "total amount of all product is #{@amount.values.sum}" 
+    end
+  end
+end
+
+prc = Purchase.new
+
+
+prc.add_in_cart
+prc.create_amount
+prc.get_amount
+puts prc.get_amount_sum
