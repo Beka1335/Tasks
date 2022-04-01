7ce0548d92a50c0c61e9e258ffa8c6f69ecafd8f
Balu <barishvili@unisens.ge>
2022-04-01 14:25:52 +0400

    added check on zero

--- Task_One/Second_task.rb
+++ Task_One/Second_task.rb
@@ -2,12 +2,30 @@ AREA = 2
 
 class Triangle
 
-  def area(base, height)
-    puts (base * height) / AREA
+  def initialize(base, height)
+    @base = base
+    @height = height
+  end
+
+  def calculate_area_of_triangle
+    if check_on_zero
+      puts (@base * @height) / AREA
+    else
+      puts "one of the argument is zero"
+    end
+    
+  end
+
+  def check_on_zero
+    !(@base.zero? || @height.zero?)
   end
 end
 
+
+puts "Enter triangle base length:"
 base = gets.chomp.to_f
+puts "Enter triangle height length:"
 height = gets.chomp.to_f
 
-puts Triangle.new.area(base, height) 
+triangle = Triangle.new(base, height)
+puts triangle.calculate_area_of_triangle
