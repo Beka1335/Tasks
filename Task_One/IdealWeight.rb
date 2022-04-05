4873325ace3cd62902b58eaf855d5fb8ed39764f
Beka1335 <100201334+Beka1335@users.noreply.github.com>
2022-04-05 16:13:43 +0400

    Update IdealWeight.rb

--- Task_One/IdealWeight.rb
+++ Task_One/IdealWeight.rb
@@ -1,52 +1,42 @@
 class CalculateIdealWeight
-  
   class AttributeError < StandardError; end
   SUBTRACTOR = 110
-  EDGE = 0
 
   def initialize
-    print "Enter Your Name: "
-    name = gets.chomp
-    print "Enter Your Height: "
-    height = gets.chomp
-
-    @name = name
-    @height = height
+    print 'Enter Your Name: '
+    @name = 'asd'.chomp
+    print 'Enter Your Height: '
+    @height = '180'.chomp
   end
 
   def call
     validate
-    calculate_ideal_weight
     ideal_weight
-  rescue AttributeError => error
-    warn error
+    detect_ideal_weight
+  rescue AttributeError => e
+    warn e
   end
 
   private
 
-  def ideal_weight
-    if @optimal_weight <= EDGE
+  def detect_ideal_weight
+    if ideal_weight.zero?
       print "#{@name} Your weight is already optimal"
     else
       print "#{@name} Your optimal weight is #{@optimal_weight}"
     end
   end
 
-  def calculate_ideal_weight
+  def ideal_weight
     @optimal_weight = @height.to_f - SUBTRACTOR
   end
 
   def validate
-    if @name.empty?
-      raise AttributeError, "please enter your name"
+      raise AttributeError, 'please enter your name' if @name.empty?
     elsif @height.match(/\D/)
       raise AttributeError, "#{@name.capitalize}, Height must be numbers"
     elsif @height.empty?
       raise AttributeError, "#{@name.capitalize}, height must not be nill"
-    elsif @height.to_f <= EDGE
-      raise AttributeError, "#{@name.capitalize}, height must be positive number"
-    else
-      return
     end
   end
 end
