d23f148ae9808eb96a317edd8679ffb32cf4ad50
Balu <barishvili@unisens.ge>
2022-04-06 17:47:16 +0400

    find duplicate in array

---
+++ week4/find_duplicate.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# find duplicate in array
+class FindDuplicateInArray
+  class AttributeError < StandardError; end
+
+  def initialize
+    @array = []
+  end
+
+  def call
+    fill_array
+    find_dublicates
+  end
+
+  private
+
+  def fill_array
+    loop do
+      puts "\n When you end filling array 'stop'"
+      number = gets.chomp
+      break if number == 'stop'
+
+      validate(number)
+      print @array << number.to_i
+    rescue AttributeError => e
+      puts e
+    end
+  end
+
+  def find_dublicates
+    @array.select { |number| @array.count(number) > 1 }.uniq
+  end
+
+  def validate(number)
+    raise AttributeError, 'input must be numbers' if number.match(/\D/)
+
+    raise AttributeError, 'input must not be nill' if number.to_s.empty?
+  end
+end
+
+p Find_Duplicate_in_array.new.call
+
+
