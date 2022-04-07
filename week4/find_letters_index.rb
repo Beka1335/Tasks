d9f4ff54e153dc01ddd4e4af452e91aa87c4efab
Balu <barishvili@unisens.ge>
2022-04-07 09:35:22 +0400

    Map the letters of a string

---
+++ week4/find_letters_index.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# find letters index in string
+class FindLettersIndex
+  class AttributeError < StandardError; end
+  REGEXSCANLETTERS = /[^a-z]/.freeze
+  COMENTFORERROR = 'The input string should contain only letters'
+
+  def initialize
+    puts 'please enter string'
+    @letters = gets.chomp
+    @letters_hash = {}
+  end
+
+  def call
+    validate
+    find_letters_index
+  rescue AttributeError => e
+    warn e
+  end
+
+  private
+
+  def find_letters_index
+    @letters.each_char.with_index do |letter, index|
+      @letters_hash[letter] ||= []
+      @letters_hash[letter] << index
+    end
+    @letters_hash
+  end
+
+  def validate
+    raise AttributeError, COMENTFORERROR if @letters.scan(REGEXSCANLETTERS)
+  end
+end
+
+puts FindLettersIndex.new.call
