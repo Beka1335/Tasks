19b37e8318d1d081fb861e9cb9b5284fc6ad707d
Balu <barishvili@unisens.ge>
2022-04-07 12:04:14 +0400

    count prime numbers

---
+++ week4/prime_number.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+# How Many Prime Numbers Are in a Given Array
+class PrimeNumber
+  class AttributeError < StandardError; end
+
+  def initialize
+    @array = []
+  end
+
+  def call
+    fill_array
+    count_prime_numbers
+  end
+
+  private
+
+  def fill_array
+    loop do
+      puts "\n When you end filling array 'stop'"
+      print 'Enter Number :  '
+      number = gets.chomp
+      break if number == 'stop'
+
+      validate(number)
+      print @array << number.to_i
+    rescue AttributeError => e
+      puts e
+    end
+  end
+
+  def count_prime_numbers
+    counted = @array.count { |number| is_prime?(number) }
+    puts "there are #{counted} prime numbers"
+  end
+
+  def is_prime?(number)
+    return false if number == 1
+
+    sqrt_number = Math.sqrt(number)
+    (2..sqrt_number).each do |divisor|
+      return false if (number % divisor).zero?
+    end
+    true
+  end
+
+  def validate(number)
+    raise AttributeError, 'input must be numbers' if number.match(/\D/)
+
+    raise AttributeError, 'input must not be nill' if number.to_s.empty?
+  end
+end
+
+PrimeNumber.new.call
+
+
